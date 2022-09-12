793b376e0d2290febe001455564422e93357c98a
Hguerrero0821 <hguerreroc0822@gmail.com>
2022-09-12 18:06:03 -0500

    actualizacion 2

--- laboratorios/Lab2/lab22.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include("noesiste.php");
-    //require("noesiste.php");
-    echo ("Hola. El script siguió!");
-?>
--- laboratorios/Lab4/Lab42/lab4.2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $numero = $_POST['num'];
-    $factorial = 1;
-    for($i = 1; $i<=$numero; $i++){
-        $factorial = $factorial  * $i;
-    }
-    echo "<br/> El factorial de ".$numero." es de: ".$factorial;
-?>
